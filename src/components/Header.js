5a67723897fa90d368627235db8af3995042942c
BrittanyBrown14 <121870967+BrittanyBrown14@users.noreply.github.com>
2023-11-01 12:14:08 +0000

    Adding the header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+function Header() {
+    return (
+      <div className="header">
+        <header className="header--menu">
+            <ul>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Github Portfolio</li>
+                <li>Skills</li>
+            </ul>
+            <button>Contact Me</button>
+        </header>
+      </div>
+    );
+  }
+
+  export default Header;
